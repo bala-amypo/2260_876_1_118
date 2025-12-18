50f8f002e572dfeef369e4e708e64b5117bc32e8
bala-amypo <bala@amypo.in>
2025-12-18 08:54:48 +0000

    User commit

--- src/main/java/com/example/demo/repository/SupplierRiskAlertRepository.java
+++ src/main/java/com/example/demo/repository/SupplierRiskAlertRepository.java
@@ -1,1 +1,11 @@
 package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.SupplierRiskAlert;
+
+@Repository
+public interface SupplierRiskAlertRepository
+        extends JpaRepository<SupplierRiskAlert, Long> {
+
+}

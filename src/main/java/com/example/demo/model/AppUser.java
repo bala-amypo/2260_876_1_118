318398ccac3925a545451f6ea915c2dc98ed55ad
bala-amypo <bala@amypo.in>
2025-12-18 08:25:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -0,0 +1,76 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "app_user", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    @ElementCollection
+    private Set<String> roles;
+
+    private LocalDateTime createdAt;
+
+    public AppUser() {
+    }
+
+    public AppUser(Long id, String email, String password,
+                   Set<String> roles, LocalDateTime createdAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}

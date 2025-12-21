76a333236ff657e4ba70b38b68ab8765bc5a1131
bala-amypo <bala@amypo.in>
2025-12-21 11:18:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/SupplierProfileRepository.java
+++ src/main/java/com/example/demo/repository/SupplierProfileRepository.java
@@ -1,13 +1,13 @@
 package com.example.demo.repository;
-import com.example.demo.model.Supplier;
+import com.example.demo.model.SupplierProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 @Repository
 
-public interface SupplierProfileRepository extends JpaRepository<Supplier, Long> {
+public interface SupplierProfileRepository extends JpaRepository<SupplierProfile, Long> {
 
-    Optional<Supplier> findBySupplierCode(String supplierCode);
+    Optional<SupplierProfile> findBySupplierCode(String supplierCode);
 
     boolean existsBySupplierCode(String supplierCode);
 }

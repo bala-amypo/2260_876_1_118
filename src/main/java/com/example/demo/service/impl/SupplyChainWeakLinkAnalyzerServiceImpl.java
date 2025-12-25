6763592dd3db0656bdfe44739e6007b38cd4bed8
bala-amypo <bala@amypo.in>
2025-12-25 14:51:36 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplyChainWeakLinkAnalyzerServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplyChainWeakLinkAnalyzerServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.demo.service.impl;
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
 import com.example.demo.service.*;
-import com.example.demo.repository.*;
+import com.example.demo.service.impl.*;
 
 import com.example.demo.service.SupplyChainWeakLinkAnalyzerService;
 import org.springframework.stereotype.Service;

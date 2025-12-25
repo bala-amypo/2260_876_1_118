a382073a28171398ad41921044dc0628345a8652
bala-amypo <bala@amypo.in>
2025-12-25 10:51:14 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,17 @@ public class DemoApplication {
 	}
 
 }
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+

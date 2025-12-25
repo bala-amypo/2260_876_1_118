8bbba7e987e6df6f6be18f15e0a54a3b9c865e6c
bala-amypo <bala@amypo.in>
2025-12-25 06:56:05 +0000

    User commit

--- src/main/java/com/example/demo/service/DeliveryRecordService.java
+++ src/main/java/com/example/demo/service/DeliveryRecordService.java
@@ -1,45 +1,11 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
-import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.DeliveryRecord;
-import com.example.demo.repository.DeliveryRecordRepository;
-import com.example.demo.repository.PurchaseOrderRecordRepository;
-import com.example.demo.service.DeliveryRecordService;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class DeliveryRecordServiceImpl implements DeliveryRecordService {
-    private final DeliveryRecordRepository deliveryRepository;
-    private final PurchaseOrderRecordRepository poRepository;
-
-    public DeliveryRecordServiceImpl(DeliveryRecordRepository deliveryRepository, 
-                                    PurchaseOrderRecordRepository poRepository) {
-        this.deliveryRepository = deliveryRepository;
-        this.poRepository = poRepository;
-    }
-
-    @Override
-    public DeliveryRecord recordDelivery(DeliveryRecord delivery) {
-        if (!poRepository.existsById(delivery.getPoId())) {
-            throw new BadRequestException("Invalid PO id");
-        }
-
-        if (delivery.getDeliveredQuantity() == null || delivery.getDeliveredQuantity() < 0) {
-            throw new BadRequestException("Delivered quantity must be >=");
-        }
-
-        return deliveryRepository.save(delivery);
-    }
-
-    @Override
-    public List<DeliveryRecord> getDeliveriesByPO(Long poId) {
-        return deliveryRepository.findByPoId(poId);
-    }
-
-    @Override
-    public List<DeliveryRecord> getAllDeliveries() {
-        return deliveryRepository.findAll();
-    }
+public interface DeliveryRecordService {
+    DeliveryRecord recordDelivery(DeliveryRecord delivery);
+    List<DeliveryRecord> getDeliveriesByPO(Long poId);
+    List<DeliveryRecord> getAllDeliveries();
 }
